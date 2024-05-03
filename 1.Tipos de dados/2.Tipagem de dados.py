8e3353b0b37a268d1c26f92dc612a0ffb505aded
Waguinhoo <156625511+Wagnersilva05@users.noreply.github.com>
2024-05-03 12:24:13 +0000

    Tipos de dados

---
+++ 1.Tipos de dados/2.Tipagem de dados.py
@@ -0,0 +1,13 @@
+nome = "Marta"
+idade = 20
+peso = 62.500
+
+
+print(nome)
+print(idade)
+print(peso)
+
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso}")
---
+++ 1.Tipos de dados/3.Tipagem estatica.py
@@ -0,0 +1,8 @@
+nome : str = "Marta"
+idade: int = 20
+peso: float = 62.500
+
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso}")
