7e972b6150e21515150f25b6700ed7244bcfa4e7
Waguinhoo <156625511+Wagnersilva05@users.noreply.github.com>
2024-05-10 10:16:48 -0300

    Delete aula10 directory

--- aula10/AulaExtra.py
+++
@@ -1,2 +0,0 @@
-nome = 'Wagner'
-print("Ola! prazer, {}{}{}".format('\033[4;34m', nome, '\033[m'))
--- aula10/exercicio32.py
+++
@@ -1,6 +0,0 @@
-ano = int(input("Digite o ano: "))
-
-if(ano % 400 == 0):
-    print("Ano bissexto")
-else:
-    print("Não é um ano bissexto")
