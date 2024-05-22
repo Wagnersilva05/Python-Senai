613bb867b2d01d7945171682fbd74d1575240b45
Waguinhoo <156625511+Wagnersilva05@users.noreply.github.com>
2024-05-22 07:50:53 -0300

    Add files via upload

---
+++ funções/ex1.py
@@ -0,0 +1,13 @@
+import os
+
+def cabecalho():
+    os.system('clear')
+    print("--------------------")
+    print("=== SENAI | FIEB ===")
+    print("--------------------")
+
+cabecalho()
+nome = input("Digite seu nome: ")
+
+cabecalho()
+idade = int("Digite sua idade: ")
